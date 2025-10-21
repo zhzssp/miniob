c91522a65ceded1b891c2aabacd07ae957328cef
LoveKurumi <1461302364@qq.com>
2025-10-22 04:15:59 +0800

    第四次提交

--- src/observer/common/type/data_type.cpp
+++ src/observer/common/type/data_type.cpp
@@ -12,6 +12,7 @@ See the Mulan PSL v2 for more details. */
 #include "common/type/float_type.h"
 #include "common/type/integer_type.h"
 #include "common/type/data_type.h"
+#include "common/type/date_type.h"
 #include "common/type/vector_type.h"
 
 // Todo: 实现新数据类型
@@ -22,6 +23,7 @@ array<unique_ptr<DataType>, static_cast<int>(AttrType::MAXTYPE)> DataType::type_
     make_unique<CharType>(),
     make_unique<IntegerType>(),
     make_unique<FloatType>(),
+    make_unique<DateType>(),
     make_unique<VectorType>(),
     make_unique<DataType>(AttrType::BOOLEANS),
 };
