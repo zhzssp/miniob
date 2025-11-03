1530964ecd5acf805525488f9f7183d6103fa813
H Z <zhzssp666@163.com>
2025-11-03 19:52:40 +0800

    test filter 22

--- src/observer/sql/optimizer/logical_plan_generator.cpp
+++ src/observer/sql/optimizer/logical_plan_generator.cpp
@@ -327,6 +327,17 @@ RC LogicalPlanGenerator::create_plan(FilterStmt *filter_stmt, unique_ptr<Logical
       right = make_unique<ValueExpr>(filter_obj_right.value);
     }
 
+    CompOp comp = filter_unit->comp();
+    if (comp == IS_OP || comp == IS_NOT_OP) {
+      // is null判断 --> 避免进入cast部分
+      if (filter_obj_left.is_null() || filter_obj_right.is_null()) {
+        LOG_INFO("Null appears, cannot implicitly cast value.");
+        ComparisonExpr *cmp_expr = new ComparisonExpr(filter_unit->comp(), std::move(left), std::move(right));
+        cmp_exprs.emplace_back(cmp_expr);
+        continue;
+      }
+    }
+
     // 跳过子查询表达式的类型检查
     // 如果 right 的 value_type 是 UNDEFINED，很可能包含子查询
     bool has_subquery = (right->value_type() == AttrType::UNDEFINED) || 
@@ -341,14 +352,6 @@ RC LogicalPlanGenerator::create_plan(FilterStmt *filter_stmt, unique_ptr<Logical
       LOG_WARN("Found subquery in filter, skipping type check. left_type=%d, right_type=%d", 
                 (int)left->type(), (int)right->type());
     }
-
-    // // is null判断 --> 避免进入cast部分
-    // if (filter_obj_left.is_null() || filter_obj_right.is_null()) {
-    //   LOG_INFO("Null appears, cannot implicitly cast value.");
-    //   ComparisonExpr *cmp_expr = new ComparisonExpr(filter_unit->comp(), std::move(left), std::move(right));
-    //   cmp_exprs.emplace_back(cmp_expr);
-    //   continue;
-    // }
 
     if (!has_subquery && left->value_type() != right->value_type()) {
       auto left_to_right_cost = implicit_cast_cost(left->value_type(), right->value_type());
